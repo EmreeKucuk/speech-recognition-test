c3cbfec98c94ddce8e237045f19231abc66fc404
Emre Küçük <emrekucuk_2003@hotmail.com>
2025-04-22 21:16:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
 import speech_recognition as sr
+import pyttsx3
 
+engine = pyttsx3.init()
 r = sr.Recognizer()
 
 while True:
     
     try:
         with sr.Microphone() as source:
+            rs.adjust_for_ambient_noise(source, duration=0.2)
+            print("Please say something...")
             print("Listening...")
             audio = r.listen(source)
             print("Recognizing...")
             text = r.recognize_google(audio, language='en-US')
             text= text.lower()
-            print(f"You said: {text}")
-            if "stop" in text:
-                print("Stopping the program.")
+            
+        if "stop" in text:
+            print("Stopping the program.")
             break
-    except:
+        else:
+            # Print the recognized text
+            print(f"You said: {text}")
+            engine.say(text)  
+            engine.runAndWait()
+            
+
+        
+    except sr.UnknownValueError:
         print("You were trying to say something, but I couldn't understand it.")
         r = sr.Recognizer()
-        
+        continue
     
